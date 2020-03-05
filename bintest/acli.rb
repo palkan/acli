7c9edbaf04e48b23afe25b0171de24494d01686e
Vladimir Dementyev <dementiev.vm@gmail.com>
2020-03-05 15:54:52 -0500

    Add integration tests with real server

--- bintest/acli.rb
+++ bintest/acli.rb
@@ -1,7 +1,63 @@
-require "open3"
 require_relative "../mrblib/acli/version"
+require "socket"
+require "json"
 
 BIN_PATH = File.join(File.dirname(__FILE__), "../mruby/bin/acli")
+
+# Skip integration tests if server is not running.
+#
+# Run server using the following command (from tha project's root):
+#
+#    ruby etc/server.rb
+#
+SERVER_RUNNING =
+  begin
+    Socket.tcp("localhost", 8080, connect_timeout: 1).close
+    true
+  rescue Errno::ECONNREFUSED, Errno::EHOSTUNREACH, SocketError
+    false
+  end
+
+class AcliProcess
+  attr_reader :process, :wio, :rio
+
+  def initialize(*args)
+    @rio, @wio = IO.pipe
+    @process = ChildProcess.build(BIN_PATH, *args)
+    process.io.stdout = process.io.stderr = @wio
+    process.duplex = true
+  end
+
+  def running
+    process.start
+    yield process
+  ensure
+    process.stop
+  end
+
+  def readline
+    line = nil
+    thread = Thread.new { line = rio.readline }
+
+    time = 0.99
+
+    loop do
+      return line if line
+      sleep 0.2
+
+      time -= 0.2
+
+      if time < 0
+        thread.terminate
+        return "<Timed out to read from the process>"
+      end
+    end
+  end
+
+  def puts(*args)
+    process.io.stdin.puts(*args)
+  end
+end
 
 assert("with invalid url") do
   output, status = Open3.capture2(BIN_PATH, "-u", "localhost:0:1:2")
@@ -25,4 +81,132 @@ assert("help") do
     output,
     "Usage: acli [options]"
   )
+end
+
+return $stdout.puts("Skip integration tests: server is not runnnig") unless SERVER_RUNNING
+
+require "childprocess"
+
+assert("connects successfully") do
+  acli = AcliProcess.new("-u", "localhost:8080")
+  acli.running do |process|
+    assert_true process.alive?, "Process failed"
+    assert_include(acli.readline, "Connected to Action Cable at ws://localhost:8080/cable")
+  end
+end
+
+assert("connects with query") do
+  acli = AcliProcess.new("-u", "localhost:8080/cable?token=secret#page=home")
+  acli.running do |process|
+    assert_true process.alive?, "Process failed"
+    assert_include(
+      acli.readline,
+      "Connected to Action Cable at ws://localhost:8080/cable?token=secret"
+    )
+  end
+end
+
+assert("subscribes to a channel") do
+  acli = AcliProcess.new("-u", "localhost:8080")
+  acli.running do |process|
+    assert_true process.alive?, "Process failed"
+    assert_include(
+      acli.readline,
+      "Connected to Action Cable"
+    )
+
+    acli.puts '\s DemoChannel'
+    assert_include(
+      acli.readline,
+      "Subscribed to DemoChannel"
+    )
+  end
+end
+
+assert("subscribes to a channel with int param") do
+  acli = AcliProcess.new("-u", "localhost:8080")
+  acli.running do |process|
+    assert_true process.alive?, "Process failed"
+    assert_include(
+      acli.readline,
+      "Connected to Action Cable"
+    )
+
+    acli.puts '\s EchoChannel param:1'
+    assert_include(
+      acli.readline,
+      "Subscribed to EchoChannel"
+    )
+
+    acli.puts '\p echo_params'
+    assert_include(
+      acli.readline,
+      {param: 1}.to_json
+    )
+  end
+end
+
+assert("subscribes to a channel with string param") do
+  acli = AcliProcess.new("-u", "localhost:8080")
+  acli.running do |process|
+    assert_true process.alive?, "Process failed"
+    assert_include(
+      acli.readline,
+      "Connected to Action Cable"
+    )
+
+    acli.puts '\s EchoChannel param:"Test"'
+    assert_include(
+      acli.readline,
+      "Subscribed to EchoChannel"
+    )
+
+    acli.puts '\p echo_params'
+    assert_include(
+      acli.readline,
+      {param: "Test"}.to_json
+    )
+  end
+end
+
+assert("performs action") do
+  acli = AcliProcess.new("-u", "localhost:8080", "-c", "EchoChannel")
+  acli.running do |process|
+    assert_true process.alive?, "Process failed"
+    assert_include(
+      acli.readline,
+      "Connected to Action Cable"
+    )
+    assert_include(
+      acli.readline,
+      "Subscribed to EchoChannel"
+    )
+
+    acli.puts '\p echo msg:"hello"'
+    assert_include(
+      acli.readline,
+      {pong: {msg: "hello"}}.to_json
+    )
+  end
+end
+
+assert("request query is supported") do
+  acli = AcliProcess.new("-u", "localhost:8080/cable?token=s3cr3t", "-c", "EchoChannel")
+  acli.running do |process|
+    assert_true process.alive?, "Process failed"
+    assert_include(
+      acli.readline,
+      "Connected to Action Cable"
+    )
+    assert_include(
+      acli.readline,
+      "Subscribed to EchoChannel"
+    )
+
+    acli.puts '\p echo_token'
+    assert_include(
+      acli.readline,
+      {token: "s3cr3t"}.to_json
+    )
+  end
 end
---
+++ etc/server.rb
@@ -0,0 +1,87 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require "bundler/inline"
+
+gemfile(true) do
+  source "https://rubygems.org"
+
+  gem "rails", "~> 6.0"
+  gem "puma", "~> 4.0"
+end
+
+require "rails"
+require "global_id"
+
+require "action_controller/railtie"
+require "action_view/railtie"
+require "action_cable/engine"
+
+require "rack/handler/puma"
+
+class TestApp < Rails::Application
+  secrets.secret_token    = "secret_token"
+  secrets.secret_key_base = "secret_key_base"
+
+  config.logger = Logger.new($stdout)
+  config.log_level = :debug
+  config.eager_load = true
+
+  config.filter_parameters << :token
+
+  initializer "routes" do
+    Rails.application.routes.draw do
+      mount ActionCable.server => "/cable"
+    end
+  end
+end
+
+module ApplicationCable
+  class Connection < ActionCable::Connection::Base
+    identified_by :id, :token
+
+    def connect
+      self.id = SecureRandom.uuid
+      self.token = request.params[:token] ||
+        request.cookies[:token] ||
+        request.headers["X-API-TOKEN"]
+    end
+  end
+end
+
+module ApplicationCable
+  class Channel < ActionCable::Channel::Base
+  end
+end
+
+ActionCable.server.config.cable = { "adapter" => "async" }
+ActionCable.server.config.connection_class = -> { ApplicationCable::Connection }
+ActionCable.server.config.disable_request_forgery_protection = true
+
+class DemoChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "demo"
+  end
+end
+
+class EchoChannel < ApplicationCable::Channel
+  def subscribed
+    stream_from "echo"
+  end
+
+  def echo_params
+    transmit params.without("channel")
+  end
+
+  def echo(data)
+    transmit pong: data.without("action")
+  end
+
+  def echo_token
+    transmit token: token
+  end
+end
+
+Rails.application.initialize!
+
+Rack::Handler::Puma.run(Rails.application, :Port => 8080)
